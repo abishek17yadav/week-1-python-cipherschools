d1de190728bbbdb7347b4e6eb9e8aab9dd41b9ab
Abishek_yadav <abhiyadav172408@gmail.com>
2022-12-12 21:21:36 +0530

    escape sequence

---
+++ 6escape_sequ.py
@@ -0,0 +1,46 @@
+# print("hello  \"world\"  world")
+# print('i am \'hii abishek')
+
+
+
+# #new line
+# # print("a")
+# # print("b")
+# print("line a\nline b")
+
+# print("name \t abishek")  #/t - to put tab
+
+
+# print("hell\blo")   #/b - to detete the next letter
+
+
+
+
+#----------------------------------------------------------------------------------------------
+
+# escape sequence as normal text
+# output= line A \n line B
+# method :1
+# print(r"line A \n line B") # put r and write anything u want to print
+# ------------------------------------------------------------------------------------
+# method :2
+# \"="
+# \\"=\
+# \\\"=\"
+# print("line A \\n line B")
+
+# output= \" \'
+# print("\\\" \\'")
+
+
+# print("this is  \\\\  double backslash")
+# print("these are /\\/\\/\\/\\ mountains")
+# print("he is \t awesome")
+# print("\\\" \\n \\t \\\'")
+
+
+
+
+
+
+
