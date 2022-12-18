460272bf497ef4ab0e21c05a9524079c1bdf1d9a
Abishek yadav <114800283+abishek17yadav@users.noreply.github.com>
2022-12-18 20:13:24 +0530

    variables

---
+++ 10variable.py
@@ -0,0 +1,27 @@
+
+#string
+name="abishek"
+print(name)
+
+#int
+name=123
+print(name)
+
+# rules in variable
+# you cannot start any variable by number
+# u can use numbers in between
+#u cannot use any special symbols
+
+a_b_c="hii"   #snake case writing
+userOneName="bye" #camel case writing
+
+
+
+# more about variable 
+# how to use in 1 line
+name,age="abishek","17"
+print(name+age)
+
+# multiple variable in 1 line
+x=y=z=1
+print(x+y+z)
