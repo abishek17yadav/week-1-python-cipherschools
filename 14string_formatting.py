454a66ef66f29d50f44f511f904f5bbf976f4f93
Abishek yadav <114800283+abishek17yadav@users.noreply.github.com>
2022-12-18 20:15:47 +0530

    string formatting

---
+++ 14string_formatting.py
@@ -0,0 +1,7 @@
+name="abi"
+age=18
+print("hello {} your age is {}".format(name,age)) #python 3
+
+print(f"hello {name} your age is {age}")  #python 3.6
+print(f"hello {name} your age is {age+6}") #u can even add in age
+# {}=are called place holders
