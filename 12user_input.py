462eff58fa5a5faf1dd26a1f41ce5edeb0757c30
Abishek yadav <114800283+abishek17yadav@users.noreply.github.com>
2022-12-18 20:14:43 +0530

    User input

---
+++ 12user_input.py
@@ -0,0 +1,15 @@
+# user input
+
+# use input functon
+name=input("Enter your name :")
+print("hello"+name)
+
+
+
+#more
+# how to take 2 input at once
+name,age=input("enter your name and age:").split(",")
+# .split - is used for 
+print(name)
+print(age)
+#u can use any thing in split like space,commas,etc to print
