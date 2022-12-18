858af05320296fc8660be6e38e19854723c209c0
Abishek yadav <114800283+abishek17yadav@users.noreply.github.com>
2022-12-18 20:14:07 +0530

    string concatenation

---
+++ 11string_concatenation.py
@@ -0,0 +1,15 @@
+#strings
+
+first_name="abishek "
+last_name="yadav"
+full_name=first_name+last_name
+print(full_name)
+print(full_name+str(3))
+print(full_name*3)
+
+# concatenation =addition of strings
+
+# rules
+# u can not add any number with strings
+# you can add any number by making the number string value
+#u can multiply with any numbers
