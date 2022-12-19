daa2e4399393466a191c26bd04be241ef85d91e6
Abishek yadav <114800283+abishek17yadav@users.noreply.github.com>
2022-12-19 19:33:45 +0530

    While loops

---
+++ 28-while-loops.py
@@ -0,0 +1,15 @@
+# loops =while
+# print("hello world ")  10 times
+
+i=1
+while i<=10:
+    print("hello world")
+    i+=1
+
+#sum of 1 -10 (or any numbers 
+total=0
+i=1
+while i<=10:
+    total+=i  #sum code
+    i+=1
+print(total)    
