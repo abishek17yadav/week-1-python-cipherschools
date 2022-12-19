64c0d690e42648ee03c5dc95b4e7c0f7b5dc1964
Abishek yadav <114800283+abishek17yadav@users.noreply.github.com>
2022-12-19 19:33:05 +0530

    in

---
+++ 27-in.py
@@ -0,0 +1,13 @@
+#in -- to check if it is there or not
+name="abishek"
+if "a" in name:
+    print("u r abishek")
+
+
+
+#check empty or not
+name=input("your name? :")
+if name:
+    print("string is not empty")
+else:
+    print("string is empty")
