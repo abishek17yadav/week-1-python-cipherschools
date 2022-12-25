45701ae33912719adc133ec9983f207b512e3ee5
Abishek yadav <114800283+abishek17yadav@users.noreply.github.com>
2022-12-25 13:31:18 +0530

    Add files via upload

---
+++ Exercise7.py
@@ -0,0 +1,10 @@
+# take a number and calculate its sum"1256'
+number=input("enter: ")
+# int(number[i]) +
+total=0
+i=0
+while i<len(number):
+    total+=int(number[i])
+    i+=11256
+    
+print(total)
