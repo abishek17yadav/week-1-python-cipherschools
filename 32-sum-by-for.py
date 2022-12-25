1213efc3fc9675ec120f45fec823904d41883f38
Abishek yadav <114800283+abishek17yadav@users.noreply.github.com>
2022-12-25 13:30:59 +0530

    Add files via upload

---
+++ 32-sum-by-for.py
@@ -0,0 +1,16 @@
+# sum from 1 to 10
+total=0       #take a variable
+for i in range(1,11):      #use for and give the range
+    total+=i      #to add use its function
+print(total)     #print it
+
+
+
+# take input from the user and print
+n=int(input("enter a number: "))
+total=0
+i=1  #1 se le ke n tak so, i=1
+while i<=n:
+    total +=i
+    i+=1
+print(total)
