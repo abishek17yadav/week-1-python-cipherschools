273df496df359c57d2f1537b5e65ca179dfa15d0
Abishek yadav <114800283+abishek17yadav@users.noreply.github.com>
2022-12-25 13:30:34 +0530

    for-loop

---
+++ 31-for-loop.py
@@ -0,0 +1,19 @@
+# Print hellow world 
+# in place "i" u can put anything
+# range-till where it will go
+for i in range(10):
+    print("Hello world")
+# 10 - 0 to 9
+
+# other way
+for i in range(10):
+    print(f"Hello world +{i}")
+
+
+for i in range(1,11): #till 1 to 10 only
+    print("Hello world")
+
+
+
+
+#sum 1 to 10
